24f974ba5bfb352876f391b6fdeb84bde86cf178
unknown <hitheshk27@gmail.com>
2020-05-22 11:06:15 +0530

    Header component with redux flow

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from '../components/Header'
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+     componentDidMount() {
+      this.props.fetchUser();
+  
+   }
+    render() {
+        return (
+          <div className='container'>
+            <BrowserRouter>
+              <div>
+                <Header />
+                <Route exact path='/' component={Landing} />
+                <Route exact path='/surveys' component={Dashboard} />
+                <Route path='/surveys/new' component={SurveyNew} />
+              </div>
+            </BrowserRouter>
+          </div>
+        );
+    }
+    }
 
-export default App;
+    export default connect(
+      null,
+      actions
+    )(App);
